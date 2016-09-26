7aa6ab55b0df873aae4d42dca0ff38b1b4e54c6a
Oscar Aaron Revilla Escalona <aaron_revilla@hotmail.com>
2016-09-26 11:55:28 -0400

    onSaveInstanceState

--- app/src/main/java/com/example/aaron/asynktask/MainActivity.java
+++ app/src/main/java/com/example/aaron/asynktask/MainActivity.java
@@ -13,7 +13,9 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
     TextView tv;
     TextView tv2;
-    int idx = 1;
+    int idx = 10;
+    boolean taskRunning = false;
+    Task2 task2;
 
 
     @Override
@@ -22,18 +24,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tv = ((TextView) findViewById(R.id.textView));
         tv2 = ((TextView) findViewById(R.id.textView2));
+        task2 = new Task2();
     }
 
     public void goButton(View view) {
+        startThread();
+    }
+
+    public void startThread(){
         tv.setText("Working ...");
         //Execute a thread in serial way
         //new MFAT().execute(idx);
         //Execute a thread in parallel way
-        Task2 task2 = new Task2();
+        taskRunning = true;
         task2.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, new Object[]{idx, tv, tv2});
         idx++;
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(taskRunning){
+            task2.cancel(true);
+        }
+        outState.putBoolean("key", taskRunning);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        taskRunning = savedInstanceState.getBoolean("key", false);
+        if(taskRunning){
+            startThread();
+        }
+    }
 
     /*
     class MFAT extends AsyncTask<Integer, Integer, String>{
